f4350cf6569fd23c088291c1310fc057ae943c7a
Iza Lopes <izalopes872@outlook.com>
2025-03-07 09:39:20 -0300

    :sparkles: feat: adicionar suporte a body-parser e novas rotas CRUD

--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get("/", (req, res, next) => {
   res.status(200).send({
     title: "MaskShop API",
-    version: "1.0.0"
+    version: "1.0.0",
   });
 });
+
+const create = router.post("/", (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).send({
+    id: id,
+    item: req.body,
+  });
+});
+
+const delete_item = router.delete("/", (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use("/", route);
+app.use("/products", create);
+app.use("/products", put);
+app.use("/products", delete_item);
 
 module.exports = app;
