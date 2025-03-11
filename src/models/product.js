c4da87f0690f83bd07bde56822bce7654bf9c57c
Iza Lopes <izalopes872@outlook.com>
2025-03-11 09:05:53 -0300

    :bug: fix: adicionar mensagem de erro ao campo slug

--- src/models/product.js
+++ src/models/product.js
@@ -1,38 +1,40 @@
-'use strict';
+"use strict";
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: [true, "O slug é obrigatório"],
+    trim: true,
+    index: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  tags: [
+    {
+      type: String,
+      required: true,
     },
-    slug: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    active: {
-        type: Boolean,
-        required: true,
-        default: true,
-    },
-    tags: [{
-        type: String,
-        required: true,
-    }]
+  ],
 });
 
 module.exports = mongoose.model("Product", schema);
