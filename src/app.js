94a83de215b25a7d5f9e2e4e20159a241ce357fa
Iza Lopes <izalopes872@outlook.com>
2025-03-13 10:48:17 -0300

    :sparkles: feat: adicionar suporte para clientes e pedidos, refatorar rotas de produtos

--- src/app.js
+++ src/app.js
@@ -16,14 +16,20 @@ mongoose
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
 const Product = require("./models/product");
+const Customer = require("./models/customer");
+const Order = require("./models/order");
 
 const indexRoutes = require("./routes/index-routes");
-const productsRoutes = require("./routes/products-routes");
+const productsRoutes = require("./routes/product-routes");
+const orderRoutes = require("./routes/order-routes");
+const customerRoutes = require("./routes/customer-routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", indexRoutes);
 app.use("/products", productsRoutes);
+app.use("/customers", customerRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;
