3697723853c7c0bb1650e4e204efd14a24647941
Iza Lopes <izalopes872@outlook.com>
2025-03-29 14:22:30 -0300

    :sparkles: feat: aprimorar e padronizar os serviços de token e autorizaçao

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -2,59 +2,66 @@
 
 const jwt = require("jsonwebtoken");
 
-exports.generateToken = async (data) => {
-  return jwt.sign(data, global.SALT_KEY, { expiresIn: "1d" });
+exports.generateToken = (data) => {
+  return jwt.sign(data, process.env.SALT_KEY, { expiresIn: "1d" });
 };
 
-exports.decodeToken = async (token) => {
-  var data = await jwt.verify(token, global.SALT_KEY);
-  return data;
+exports.decodeToken = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.SALT_KEY, (error, decoded) => {
+      if (error) {
+        reject("Token inválido");
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
 };
 
-exports.authorize = function (req, res, next) {
-  var token =
+exports.authorize = (req, res, next) => {
+  const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
 
   if (!token) {
-    res.status(401).json({
+    return res.status(401).json({
       message: "Acesso Restrito",
     });
-  } else {
-    jwt.verify(token, global.SALT_KEY, function (error, decoded) {
-      if (error) {
-        res.status(401).json({
-          message: "Token Inválido",
-        });
-      } else {
-        next();
-      }
-    });
   }
+
+  jwt.verify(token, process.env.SALT_KEY, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({
+        message: "Token Inválido",
+      });
+    }
+    req.user = decoded; 
+    next();
+  });
 };
 
-exports.isAdmin = function (req, res, next) {
-  var token =
+exports.isAdmin = (req, res, next) => {
+  const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
 
   if (!token) {
-    res.status(401).json({
+    return res.status(401).json({
       message: "Token Inválido",
     });
-  } else {
-    jwt.verify(token, global.SALT_KEY, function (error, decoded) {
-      if (error) {
-        res.status(401).json({
-          message: "Token Inválido",
-        });
-      } else {
-        if (decoded.roles.includes("admin")) {
-          next();
-        } else {
-          res.status(403).json({
-            message: "Esta funcionalidade é restrita para administradores",
-          });
-        }
-      }
-    });
   }
+
+  jwt.verify(token, process.env.SALT_KEY, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({
+        message: "Token Inválido",
+      });
+    }
+    if (decoded.roles.includes("admin")) {
+      req.user = decoded; 
+      next();
+    } else {
+      return res.status(403).json({
+        message: "Esta funcionalidade é restrita para administradores",
+      });
+    }
+  });
 };
