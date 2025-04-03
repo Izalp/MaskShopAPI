e4b58a8ad0f18cae3466d2902b1cd0d27e9aa508
Iza Lopes <izalopes872@outlook.com>
2025-04-03 20:46:27 -0300

    :sparkles: feat: adicionar script para envio de notificação de execução do pipeline

---
+++ scripts/send_notification.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const emailService = require("../services/emailService");
+const recipientEmail = process.env.EMAIL_RECIPIENT;
+
+const subject = "Notificação: Pipeline Executado";
+const body = "<strong>O pipeline foi executado com sucesso.</strong>";
+
+emailService.send(recipientEmail, subject, body)
+  .then(() => {
+    console.log("E-mail enviado com sucesso.");
+  })
+  .catch((error) => {
+    console.error("Erro ao enviar e-mail:", error);
+    process.exit(1); 
+  });
