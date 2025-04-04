9c5e23d94688906b4a86552b4d99b1e01af82d0a
Iza Lopes <izalopes872@outlook.com>
2025-04-03 21:05:32 -0300

    :recycle: refactor: adapta serviço de e-mail para nova biblioteca SendGrid

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,13 +1,19 @@
-"use strict";
+const sgMail = require('@sendgrid/mail');
 
-var config = require("../config");
-var sendgrid = require("sendgrid")(config.sendgridKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.send = async (to, subject, body) => {
-  sendgrid.send({
+  const msg = {
     to: to,
-    from: "hello@maskshop.io",
+    from: 'hello@maskshop.io', 
     subject: subject,
     html: body,
-  });
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log('E-mail enviado com sucesso.');
+  } catch (error) {
+    console.error('Erro ao enviar e-mail:', error);
+  }
 };
